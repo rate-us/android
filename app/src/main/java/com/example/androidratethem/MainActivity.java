3d9bb53007ee418f361496c0215841de374a2baa
Jhalaa <jhalaachinoy@gmail.com>
2019-04-08 10:45:39 -0700

    Adding signIn button and method for same

--- app/src/main/java/com/example/androidratethem/MainActivity.java
+++ app/src/main/java/com/example/androidratethem/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.androidratethem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void signIn(View view) {
+
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder().build(),
+                RC_SIGN_IN
+        );
     }
 }
