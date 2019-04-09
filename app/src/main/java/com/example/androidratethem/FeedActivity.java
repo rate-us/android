ed4395764f38dd529106ecb8604bb4bc35d376ad
Jhalaa <jhalaachinoy@gmail.com>
2019-04-09 05:04:11 -0700

    adding get upload image and activity results

--- app/src/main/java/com/example/androidratethem/FeedActivity.java
+++ app/src/main/java/com/example/androidratethem/FeedActivity.java
@@ -1,10 +1,21 @@
 package com.example.androidratethem;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class FeedActivity extends AppCompatActivity {
+
+    // arbitrary numbers to start respective intents
+    static final int RC_PERMISSION_READ_EXTERNAL_STORAGE = 1;
+    static final int RC_IMAGE_GALLERY = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +24,30 @@ public class FeedActivity extends AppCompatActivity {
     }
 
     public void uploadImage(View view) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, RC_PERMISSION_READ_EXTERNAL_STORAGE);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(intent, RC_IMAGE_GALLERY);
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == RC_PERMISSION_READ_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, RC_IMAGE_GALLERY);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_IMAGE_GALLERY && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+        }
     }
 }
