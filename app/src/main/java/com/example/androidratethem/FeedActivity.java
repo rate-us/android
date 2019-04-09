26933ac717e721236869739a07c9750c4422c232
Jhalaa <jhalaachinoy@gmail.com>
2019-04-09 05:18:32 -0700

    save uploaded image to firebase

--- app/src/main/java/com/example/androidratethem/FeedActivity.java
+++ app/src/main/java/com/example/androidratethem/FeedActivity.java
@@ -10,17 +10,31 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FeedActivity extends AppCompatActivity {
 
     // arbitrary numbers to start respective intents
     static final int RC_PERMISSION_READ_EXTERNAL_STORAGE = 1;
     static final int RC_IMAGE_GALLERY = 2;
+    FirebaseUser fbUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        fbUser = FirebaseAuth.getInstance().getCurrentUser();
     }
 
     public void uploadImage(View view) {
@@ -48,6 +62,27 @@ public class FeedActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == RC_IMAGE_GALLERY && resultCode == RESULT_OK) {
             Uri uri = data.getData();
+            StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+            StorageReference imagesRef = storageRef.child("images");
+            StorageReference userRef = imagesRef.child(fbUser.getUid());
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String filename = fbUser.getUid() + "_" + timeStamp;
+            StorageReference fileRef = userRef.child(filename);
+
+            UploadTask uploadTask = fileRef.putFile(uri);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    Toast.makeText(FeedActivity.this, "Upload failed!\n" + exception.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    Toast.makeText(FeedActivity.this, "Upload finished!", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
